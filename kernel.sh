457260070f3b3efb2342cfe519e3f46e5145e7f3
tesla59 <nishant.maintainers@pixysos.com>
2020-09-03 15:48:35 +0530

    kernel: rewrire

--- kernel.sh
+++ kernel.sh
@@ -1,20 +1,23 @@
 #!/bin/bash
  # Copyright (c) 2020 Tesla59 <talktonishantsingh.ns@gmail.com>
 
-# Export Your Telegram configs here
-# For security concern, dont push your chat ID and token publically
-export MSG_URL="https://api.telegram.org/bot$token/sendMessage"
-export BUILD_URL="https://api.telegram.org/bot$token/sendDocument"
+#Configs
+ID=""
+token=""
+MAKECLEAN=1
+MAKEDTBO=1
 
-# Function to post messages to telegram
+
+############################################################################################################################
+
+MSG_URL="https://api.telegram.org/bot$token/sendMessage"
+BUILD_URL="https://api.telegram.org/bot$token/sendDocument"
 post_msg() {
         curl -s -X POST "$MSG_URL" -d chat_id="$ID" \
         -d "disable_web_page_preview=true" \
         -d "parse_mode=html" \
         -d text="$1"
 }
-
-# Function to post Docs to telegram
 post_doc() {
         curl --progress-bar -F document=@"$1" "$BUILD_URL" \
         -F chat_id="$ID"  \
@@ -23,57 +26,65 @@ post_doc() {
         -F caption="$2"
 }
 
-## Make Clean Build or not##
-MAKECLEAN=1
-## Build With Dtbo or not##
-MAKEDTBO=1
+clone_tc()  {
+	git clone https://android.googlesource.com/platform/prebuilts/gcc/linux-x86/aarch64/aarch64-linux-android-4.9 --depth=1 gcc
+	git clone https://android.googlesource.com/platform/prebuilts/gcc/linux-x86/arm/arm-linux-androideabi-4.9 --depth=1 gcc32
+	git clone https://android.googlesource.com/platform/prebuilts/clang/host/linux-x86 --depth=1 clang
+	git clone https://android.googlesource.com/platform/system/libufdt scripts/ufdt/libufdt
+}
 
-# Cleaning Everything
-if [ $MAKECLEAN = 1 ] || [ "$MAKECLEAN" = "y" ]
-then
+makeclean() {
 	make O=out clean
 	make O=out mrproper
 	rm -rf out zipper
 	mkdir out
-fi
+}
+compile() {
+	post_msg "<code>Compilation started for StromBreaker Kernel</code>"
+	BUILD_START=$(date +"%s")
+	make O=out ARCH=arm64 vendor/violet-perf_defconfig
+	make -j$(nproc --all) O=out ARCH=arm64 CC="$(pwd)/clang/clang-r370808/bin/clang" CLANG_TRIPLE="aarch64-linux-gnu-" CROSS_COMPILE="$(pwd)/gcc/bin/aarch64-linux-android-" CROSS_COMPILE_ARM32="$(pwd)/gcc32/bin/arm-linux-androideabi-" | tee full.log
+	BUILD_END=$(date +"%s")
+	DIFF=$((BUILD_END - BUILD_START))
+}
 
-# Cloning GCC and CLANG
-git clone https://android.googlesource.com/platform/prebuilts/gcc/linux-x86/aarch64/aarch64-linux-android-4.9 --depth=1 gcc
-git clone https://android.googlesource.com/platform/prebuilts/gcc/linux-x86/arm/arm-linux-androideabi-4.9 --depth=1 gcc32
-git clone https://android.googlesource.com/platform/prebuilts/clang/host/linux-x86 --depth=1 clang
-git clone https://android.googlesource.com/platform/system/libufdt scripts/ufdt/libufdt
-git clone https://github.com/tesla59/AnyKernel3 zipper
-
-# Exports
-export KBUILD_BUILD_HOST="veronica"
-export KBUILD_BUILD_USER="tesla"
-export KBUILD_JOBS="$((`grep -c '^processor' /proc/cpuinfo`))"
-export ARCH=arm64 && export SUBARCH=arm64
-
-# Here we go
-BUILD_START=$(date +"%s")
-post_msg "<code>Compilation started for StromBreaker Kernel</code>"
-make O=out ARCH=arm64 vendor/violet-perf_defconfig
-make -j$(nproc --all) O=out ARCH=arm64 CC="$(pwd)/clang/clang-r370808/bin/clang" CLANG_TRIPLE="aarch64-linux-gnu-" CROSS_COMPILE="$(pwd)/gcc/bin/aarch64-linux-android-" CROSS_COMPILE_ARM32="$(pwd)/gcc32/bin/arm-linux-androideabi-" | tee full.log
-
-# Building DTBO
-if [ $MAKEDTBO = 1 ] || [ "$MAKEDTBO" = "y" ]
-then
+makedtbo() {
 	python2 "scripts/ufdt/libufdt/utils/src/mkdtboimg.py" \
 	create "out/arch/arm64/boot/dtbo.img" --page_size=4096 "out/arch/arm64/boot/dts/xiaomi/violet-sm6150-overlay.dtbo"
 	cp out/arch/arm64/boot/dtbo.img zipper
-fi
-BUILD_END=$(date +"%s")
-DIFF=$((BUILD_END - BUILD_START))
+}
 
-# Refining the kernel
-if [ -f out/arch/arm64/boot/Image.gz-dtb ]
-then
+error() {
+	post_doc "full.log" "Build failed after $(($DIFF / 60)) mins and $(($DIFF % 60)) Second(s)"
+}
+
+zip() {
 	file="stormbreaker-$(TZ=Asia/Kolkata date +'%Y%m%d-%H%M')-dtbo.zip"
 	cp out/arch/arm64/boot/Image.gz-dtb zipper
 	cd zipper
 	zip -r9 $file * -x README.md $file
 	post_doc "$file" "Build Completed in $((DIFF / 60)) minute(s) and $((DIFF % 60)) second(s)"
+}
+
+################################################################################################################
+
+clone_tc > /dev/null
+
+if [ $MAKECLEAN = 1 ]
+then
+	makeclean
+fi
+
+compile
+
+if [ $MAKEDTBO = 1 ]
+then
+	makedtbo
+fi
+
+if [ -f out/arch/arm64/boot/Image.gz-dtb ]
+then
+	zip
 else
-	post_doc "full.log" "Build failed after $(($DIFF / 60)) mins and $(($DIFF % 60)) Second(s)"
+	error
 fi
