b1dfa724fa82d6bce54aad22597ceb2cb710a519
tesla59 <talktonishantsingh.ns@gmail.com>
2022-06-05 17:26:30 +0530

    update server_setup use standard function definition fix if statement use double quote to prevent globbing

--- server_setup.sh
+++ server_setup.sh
@@ -1,3 +1,4 @@
+#!/bin/sh
 # Server Setup Script
 
 USER="tesla"
@@ -9,36 +10,36 @@ THEME="jonathan"
 # Setup Plugins git sudo zsh-interactive-cd zsh-autosuggestions zsh-syntax-highlighting
 # Setup packages
 
-function CreateUser {
+CreateUser() {
     sudo useradd -U -m -d '/home/$USER' -s '/bin/zsh' $USER
     sudo usermod -aG sudo $USER
 }
 
-function SetupSSH {
+SetupSSH() {
     mkdir -p /home/$USER/.ssh
     touch /home/$USER/.ssh/authorized_keys
-    cat $HOME/.ssh/authorized_keys >> /home/$USER/.ssh/authorized_keys
+    cat "$HOME"/.ssh/authorized_keys >> /home/$USER/.ssh/authorized_keys
 }
 
-function SetupZsh {
+SetupZsh() {
     sudo apt update -y
     sudo apt upgrade -y
     sudo apt install zsh -y
     wget https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh
     sudo -u $USER sh install.sh
-    sed -i 's/robbyrussell/$THEME/g' /home/$USER/.zshrc
+    sed -i "s/robbyrussell/$THEME/g" /home/$USER/.zshrc
 }
 
-function ClonePlugins {
+ClonePlugins() {
     # TODO: Add sudo zsh-interactive-cd zsh-syntax-highlighting zsh-autosuggestions to .zshrc
     
     # zsh-syntax-highlighting
-    git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+    git clone https://github.com/zsh-users/zsh-syntax-highlighting.git "${ZSH_CUSTOM:-~/.oh-my-zsh/custom}"/plugins/zsh-syntax-highlighting
     # zsh-autosuggestions
-    git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+    git clone https://github.com/zsh-users/zsh-autosuggestions "${ZSH_CUSTOM:-~/.oh-my-zsh/custom}"/plugins/zsh-autosuggestions
 }
 
-function SetupPackages {
+SetupPackages() {
     # Caddy
     sudo apt install -y debian-keyring debian-archive-keyring apt-transport-https
     curl -1sLf 'https://dl.cloudsmith.io/public/caddy/stable/gpg.key' | sudo gpg --dearmor -o /usr/share/keyrings/caddy-stable-archive-keyring.gpg
@@ -51,18 +52,18 @@ function SetupPackages {
 }
 
 # Create User and add to sudoer group
-if [[ $(cat /etc/passwd) =~ $USER ]]; then
+if [ "$(cat /etc/passwd)" =~ $USER ]; then
     # Create a new User
-    CreateUser()
-    SetupSSH()
+    CreateUser
+    SetupSSH
 fi
 
 # Setup ZSH
-SetupZsh()
+SetupZsh
 
 # Clone all plugins
-ClonePlugins()
+ClonePlugins
 
 # Setup Packages
-SetupPackages()
+SetupPackages
 
